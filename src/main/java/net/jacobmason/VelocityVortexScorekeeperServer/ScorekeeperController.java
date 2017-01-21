b3fe683660e693c20b0de24f704163d990fc3994
Jacob Mason <jacob@jacobmason.net>
2017-01-21 16:02:09 -0600

    Communicates with android devices via Websockets

--- src/main/java/net/jacobmason/VelocityVortexScorekeeperServer/ScorekeeperController.java
+++ src/main/java/net/jacobmason/VelocityVortexScorekeeperServer/ScorekeeperController.java
@@ -1,18 +1,12 @@
 package net.jacobmason.VelocityVortexScorekeeperServer;
 
 import net.jacobmason.VelocityVortexScorekeeperServer.messages.ScoreMessage;
-import org.springframework.http.HttpStatus;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -33,34 +27,17 @@ public class ScorekeeperController {
         return scorekeeper.getScoresAsSendableList();
     }
 
+    @MessageMapping("/scores")
+    public void submitScores(List<ScoreMessage> scores) {
+        for (ScoreMessage score : scores) {
+            scorekeeper.updateScore(score);
+        }
+    }
+
     @MessageMapping("/scores/reset")
     @SendTo("/topic/scores")
     public List<ScoreMessage> reset() {
         scorekeeper.clearScores();
         return scorekeeper.getScoresAsSendableList();
-    }
-
-    @PostMapping(value = "/scorekeeper/submit", produces = "application/json")
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public JsonResponse submitScore(@RequestBody ScoreMessage score) {
-        messenger.convertAndSend("/topic/scores", Arrays.asList(score));
-        scorekeeper.updateScore(score);
-        return new JsonResponse();
-    }
-
-    private class JsonResponse {
-        String status = "ok";
-
-        public JsonResponse() {
-        }
-
-        public JsonResponse(String status) {
-            this.status = status;
-        }
-
-        public String getStatus() {
-            return status;
-        }
     }
 }
